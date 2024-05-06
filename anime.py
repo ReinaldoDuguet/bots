e3a88aa3b506f5d9ca30b55791c56a511e241380
rduguet <reinaldo@fraccion.cl>
2024-05-06 01:07:54 -0400

    basic and functionally script

---
+++ anime.py
@@ -0,0 +1,51 @@
+import os
+from selenium import webdriver 
+from selenium.webdriver import ActionChains
+from bs4 import BeautifulSoup as bs
+import csv
+
+from lxml import html
+import time
+from datetime import datetime
+
+
+URL = "https://www3.animeflv.net/"
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+driver.get(URL)
+
+#ACTIONS
+actions = ActionChains(driver=driver)
+
+def csvFunc(datos_header, datos_json):
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    nombre = f'/csv/animes_actuales_{timestamp}.csv'
+
+    with open(nombre, 'w',newline='', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(datos_header)
+        #writer.writerows(datos_json)
+        writer.writerows([[dato] for dato in datos_json])
+
+try:
+    time.sleep(5)
+    
+    html = driver.page_source
+    soup = bs(html, 'lxml')
+    time.sleep(3)
+
+    listado_animes = soup.find('div',{'class':'mCustomScrollBox mCS-light mCSB_vertical mCSB_inside'}).text.strip().split("\n")
+    #print(listado_animes,len(listado_animes))
+    
+    datos_header = ["Nombre del Animé"]
+    datos_json = []
+    #lista_aCsv = []
+    #for anime in listado_animes:
+    #   lista_aCsv.append(anime)
+    
+    csvFunc(datos_header,listado_animes)
+
+except Exception as e :
+    f"Error - {e}"
+driver.close()
--- m_bridge.py
+++ m_bridge.py
@@ -1,14 +1,15 @@
 import os
-from selenium import webdriver 
-from selenium.webdriver import ActionChains
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs 
+from selenium.webdriver import ActionChains, Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+#from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait 
 
-import requests
-from lxml import html
+#import requests
+#from lxml import html
 import time
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,10 +18,17 @@ PASS = os.getenv('PASS_MPUENTES')
 
 
 URL = "https://oficinajudicialvirtual.pjud.cl/home/"
-path_driver ="/home/rduguet/Escritorio/proyectos_python/pjudScraping/drivers/"
 
-driver = webdriver.Firefox()
-driver.maximize_window()
+#enabling the background mode
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+options.page_load_strategy = 'none'
+#driver = webdriver.Firefox(options=options)
+driver = Firefox(options=options)
+driver.implicitly_wait(3)
+
+#driver = webdriver.Firefox()
+#driver.maximize_window()
 driver.get(URL)
 
 #ACTIONS
@@ -33,6 +41,7 @@ try:
     boton_1a = driver.find_element(By.XPATH, '/html/body/div[9]/div/section[1]/div/div[3]/div/div[1]/div/div/a[1]')
     time.sleep(5)
     
+    #posar mouse/cursor encima
     actions.move_to_element(boton_1a).perform()
     boton_1a.click()
     time.sleep(3)
@@ -47,20 +56,48 @@ try:
     ingresar_button = driver.find_element(By.XPATH, '//*[@id="login-submit"]').click()
     #FIN LOGGING
     
-    #ingreso apartado causas
+    # ingreso apartado causas
     mis_causas_button = driver.find_element(By.XPATH, '/html/body/div[1]/nav/ul/li[1]/a').click()
     
-    #civil
-    time.sleep(3)
-    civil = driver.find_element(By.XPATH, '//*[@id="civilTab"]').click()
-    
-    #Principal 
-    time.sleep(3)
-    #evaluar hacer un ciclo FOR
-    principal_activa = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[2]/div[2]/div/div/section/div/div/div[2]/div[3]/div/div/div[2]/div/div/table/tbody/tr[1]/td[1]/a/i').click()
-    print(principal_activa)
-    
+    # ingreso apartado civil
     time.sleep(5)
+    civil = driver.find_element(By.XPATH, '//*[@id="civilTab"]').click()
+
+    time.sleep(7)
+
+    html = driver.page_source
+    soup = bs(html,'lxml')
+    time.sleep(5)
+
+    #causas = soup.find('tbody',{'id':'verDetalleMisCauCiv'}).text
+    causas = soup.find('table',{'id':"dtaTableDetalleMisCauCiv"}).text.strip()
+    
+    rit_causa = 'C-163-2021'
+    estado_causa = 'Tramitación'
+    cuaderno = 'Principal'
+
+    if (estado_causa and cuaderno)  in causas:
+        print(f"el rit de la causa es: {rit_causa} y esta en la posicion {causas.index(rit_causa)}")
+        time.sleep(3)
+        boton_causa = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/section/div/div/div[2]/div[3]/div/div/div[2]/div/div/table/tbody/tr[2]/td[1]/a/i').click()
+        time.sleep(3)
+
+        tabla_causa = soup.find('div',{'class':'anel panel-default'})
+        print(f"la info de la causa es: {tabla_causa}")
+        try:
+            if '104':
+                print("el ultimo estado de la causa es 104")
+        except:
+            print("no se encontró el ultimo estado de la causa o bien se cayó esta wea")
+
+    else:
+        print("lo siento, algo pasó en el proceso y no pude encontrar el elemento o BIEN LA CAUSA NO SE ENCUENTRA")
+    #principal_activa = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[2]/div[2]/div/div/section/div/div/div[2]/div[3]/div/div/div[2]/div/div/table/tbody/tr[1]/td[1]/a/i').click()
+    
+    time.sleep(7)
+
+
 except Exception as e :
     f"Error - {e}"
 driver.close()
+#driver.quit()
